481435b7c9b7236c06df61bffb1161d7bbe0f7c8
Jared Hall <halljared1@gmail.com>
2021-11-28 05:26:34 -0500

    add paged queries, some options pipelines

---
+++ import_csv.js
@@ -0,0 +1,109 @@
+const { MongoClient } = require('mongodb');
+const fs = require('fs');
+const parse = require('csv-parse').parse;
+const db_string = 'mongodb://localhost:27017';
+const client = new MongoClient(db_string);
+const dbName = 'mtg_card_tool';
+const oracleCards = 'oracle_cards';
+const playerCards = 'player_cards';
+let db = null;
+let oracleCollection = null;
+let playerCollection = null;
+
+const onlyUnique = (val, i, self) => {
+  return self.indexOf(val) === i;
+};
+
+const processFile = async () => {
+  const parser = fs
+    .createReadStream(`./master.csv`)
+    .pipe(parse({
+    columns: true
+    }));
+  for await (const record of parser) {
+    const set = record['Set Code'].toLowerCase(),
+      num = record['Card Number'];
+    oracleCollection.findOne({set, collector_number:num})
+      .then(async (result) => {
+        if(result) {
+          const prices = result.prices;
+          let types = result.type_line;
+          const props = [
+            "name",
+            "mana_cost",
+            "cmc",
+            "oracle_text",
+            "colors",
+            "color_identity",
+            "keywords",
+            "set",
+            "set_name",
+            "collector_number",
+            "rarity"
+          ];
+          let merged = {};
+          for(let prop of props) {
+            merged[prop] = result[prop];
+          }
+          let added = {
+            quantity: record["Quantity"],
+            printing: record["Printing"],
+            language: record["Language"]
+          };
+          merged = Object.assign(merged, added);
+          let price = merged.printing == "Foil" ? prices.usd_foil : prices.usd;
+          const multiTypes = types.split('//');
+          let superTypes = [];
+          let subTypes = [];
+          for (let type of multiTypes) {
+            type = type.trim();
+            let supers = [];
+            let subs = [];
+            if (type.indexOf("—") > 0) {
+              supers = type.split("—")[0];
+            } else {
+              supers = type;
+            }
+            supers = supers.split(" ").filter((split) => {
+              return split.length > 0 && split != " ";
+            });
+
+            if (type.indexOf("—") > 0) {
+              subs = type.split("—")[1];
+            } else {
+              subs = " ";
+            }
+            subs = subs.split(" ").filter((split) => {
+              return split.length > 0 && split != " ";
+            });
+            superTypes = superTypes.concat(supers);
+            subTypes = subTypes.concat(subs);
+          }
+          merged["superTypes"] = superTypes.filter(onlyUnique);
+          merged["subTypes"] = subTypes.filter(onlyUnique);
+          merged["price"] = parseFloat(price);
+          console.log(merged);
+          playerCollection.insertOne(merged);
+        } else {
+          // TODO: fix these broken imports
+          // console.log(record);
+        }
+      });
+  }
+};
+
+async function connect() {
+  await client.connect();
+  console.log('Connected successfully to database');
+  db = client.db(dbName);
+  oracleCollection = db.collection(oracleCards);
+  playerCollection = db.collection(playerCards);
+}
+
+connect()
+  .then(() => {
+    processFile();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
+const supersPipeline = require('./pipelines/superTypes.json');
+const subsPipeline = require('./pipelines/subTypes.json');
 const port = 3000;
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 const dbName = 'mtg_card_tool';
-const collectionName = 'oracle_cards';
+const collectionName = 'player_cards';
 let db = null;
 let collection = null;
+app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    //intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+      //respond with 200
+      res.send(200);
+    }
+    else {
+    //move on
+      next();
+    }
+});
 
 async function connect() {
   await client.connect();
@@ -24,20 +42,47 @@ connect()
   .catch((err) => {
     console.error(err);
   });
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.get('/fetch_card/set/:set/number/:number', (req, res) => {
   const set = req.params.set,
     num = req.params.number;
   collection.findOne({set, collector_number:num})
     .then((result) => {
-      res.header("Access-Control-Allow-Origin", "*");
       res.json(result);
     });
 });
 
+app.get('/cards/all', (req, res) => {
+  collection.find().toArray((err, docs) => {
+    res.json(docs);
+  });
+});
+app.get('/options/supertypes', (req, res) => {
+  collection.aggregate(supersPipeline).toArray((err, docs) => {
+    res.json(docs[0].set);
+  });
+});
+app.get('/options/subtypes', (req, res) => {
+  collection.aggregate(subsPipeline).toArray((err, docs) => {
+    res.json(docs[0].set);
+  });
+});
+
+app.post('/cards/page', (req, res) => {
+  const tableOptions = req.body.tableOptions,
+    itemsPerPage = tableOptions.itemsPerPage,
+    page = tableOptions.page,
+    sortBy = tableOptions.sortBy[0],
+    sortAsc = !tableOptions.sortDesc[0],
+    sort = {};
+    if(sortBy) {
+      sort[sortBy] = sortAsc ? 1 : -1;
+    }
+  collection.find().sort(sort).skip(itemsPerPage * (page - 1)).limit(itemsPerPage).toArray((err, docs) => {
+    res.json(docs);
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
